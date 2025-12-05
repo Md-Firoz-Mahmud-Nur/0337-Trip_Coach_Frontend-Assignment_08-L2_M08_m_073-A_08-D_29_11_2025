ef3723aa20e668d44e1e265650cbc757203d1c9a
firoz.nur <firoznur5@gmail.com>
2025-12-05 08:49:50 +0600

    Add : Store

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Provider } from "react-redux";
+import { store } from "@/store/store";
 
 export const metadata: Metadata = {
   title: "Trip Coach",
@@ -14,8 +16,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <h1 className="text-9xl bg-red-500">Trip Coach</h1>
-        {children}
+        <Provider store={store}>
+          <Header />
+          <main className="max-w-6xl mx-auto p-4">{children}</main>
+        </Provider>
       </body>
     </html>
   );
