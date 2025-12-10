6b8d4ce4b1770e6576ce61f0c84c1737b3db9493
firoz.nur <firoznur5@gmail.com>
2025-12-10 10:47:20 +0600

    Start : Scratch

--- src/lib/cookieStorage.ts
+++
@@ -1,9 +0,0 @@
-import Cookies from "js-cookie";
-
-export const cookieStorage = {
-  getItem: (name: string) => Cookies.get(name) || null,
-  setItem: (name: string, value: string) => {
-    Cookies.set(name, value, { expires: 7, path: "/" });
-  },
-  removeItem: (name: string) => Cookies.remove(name),
-};
