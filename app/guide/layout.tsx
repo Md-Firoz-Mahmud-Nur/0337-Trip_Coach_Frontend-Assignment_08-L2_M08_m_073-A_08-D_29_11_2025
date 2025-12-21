292fa0e5167ea10c1857d23e0599f265d27e06dc
firoz.nur <firoznur5@gmail.com>
2025-12-21 16:33:23 +0600

    Add : Guide Protected Route

--- app/guide/layout.tsx
+++ app/guide/layout.tsx
@@ -1,4 +1,6 @@
+"use client";
 import SidebarGuide from "@/components/SidebarGuide";
+import { useRoleGuard } from "@/hooks/useRoleGuard";
 import type React from "react";
 
 export default function UserLayout({
@@ -6,12 +8,15 @@ export default function UserLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <div className="bg-background flex min-h-screen flex-col">
-      <div className="flex flex-1">
-        <SidebarGuide />
-        <main className="bg-background flex-1 overflow-auto">{children}</main>
+  useRoleGuard({ allowedRoles: ["GUIDE"], redirectTo: "/" });
+  {
+    return (
+      <div className="bg-background flex min-h-screen flex-col">
+        <div className="flex flex-1">
+          <SidebarGuide />
+          <main className="bg-background flex-1 overflow-auto">{children}</main>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
